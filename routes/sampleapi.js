9f4eb0f8f1763386b7acb52f08521e7d2cf7259f
aftab-hassan <aftabh@uw.edu>
2018-05-07 00:18:56 +0000

    will start with the visualize api

--- routes/sampleapi.js
+++ routes/sampleapi.js
@@ -8,21 +8,87 @@ Log = require('../models/log');
 
 router.get('/', function(req, res){
     console.log('aftab : came inside the /sampleapi api')
+    // res.send(JSON.stringify({
+    //     "cols": [
+    //         {"id":"","label":"Topping","pattern":"","type":"string"},
+    //         {"id":"","label":"Slices","pattern":"","type":"number"}
+    //     ],
+    //     "rows": [
+    //         {"c":[{"v":"Mushroooooms","f":null},{"v":3,"f":null}]},
+    //         {"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
+    //         {"c":[{"v":"Olives","f":null},{"v":2,"f":null}]},
+    //         {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
+    //         {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
+    //     ]
+    // }))
+
+    // res.send({
+    //     "name":"aftab",
+    //     "last":"hassan"
+    // })
+
+    // res.send({
+    //     "cols": [
+    //         {"id":"","label":"Topping","pattern":"","type":"string"},  // x-axis
+    //         {"id":"","label":"Slices 1","pattern":"","type":"number"},   // y-axis - series 0 - line 1
+    //         {"id":"","label":"Slices 2","pattern":"","type":"number"},   // y-axis - series 1 - line 2
+    //         {"id":"","role":"certainty","type":"boolean"},             // certainty role - false = dotted
+    //         {"id":"","label":"Slices 3","pattern":"","type":"number"}   // y-axis - series 2 - line 3
+    //     ],
+    //     "rows": [
+    //         {"c":[{"v":"Mushrooms","f":null},{"v":3,"f":null},{"v":4,"f":null},{"v":5,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Onions","f":null},{"v":1,"f":null},{"v":2,"f":null},{"v":3,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Olives","f":null},{"v":2,"f":null},{"v":3,"f":null},{"v":4,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null},{"v":2,"f":null},{"v":3,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null},{"v":3,"f":null},{"v":4,"f":null},{"v":false}]}
+    //     ]
+    // })
+
+    // res.send({
+    //     "cols": [
+    //         {"id":"","label":"Topping","pattern":"","type":"string"},  // x-axis
+    //
+    //         {"id":"","role":"certainty","type":"boolean"},             // certainty role - false = dotted
+    //         {"id":"","label":"Workout-Expectation","pattern":"","type":"number"},   // y-axis - series 0 - line 1
+    //
+    //         {"id":"","label":"Workout-Reality","pattern":"","type":"number"},   // y-axis - series 1 - line 2
+    //
+    //         {"id":"","role":"certainty","type":"boolean"},             // certainty role - false = dotted
+    //         {"id":"","label":"Node-Expectation","pattern":"","type":"number"},   // y-axis - series 2 - line 3
+    //
+    //         {"id":"","label":"Node-Reality","pattern":"","type":"number"}   // y-axis - series 2 - line 4
+    //     ],
+    //     "rows": [
+    //         {"c":[{"v":"Week 17","f":null},{"v":6,"f":null},{"v":5,"f":null},{"v":4,"f":null},{"v":3,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Week 18","f":null},{"v":12,"f":null},{"v":11,"f":null},{"v":8,"f":null},{"v":7,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Week 19","f":null},{"v":18,"f":null},{"v":15,"f":null},{"v":12,"f":null},{"v":12,"f":null},{"v":false}]},
+    //         {"c":[{"v":"Week 20","f":null},{"v":24,"f":null},{"v":20,"f":null},{"v":16,"f":null},{"v":14,"f":null},{"v":false}]}
+    //     ]
+    // })
+
     res.send({
         "cols": [
             {"id":"","label":"Topping","pattern":"","type":"string"},
-            {"id":"","label":"Slices","pattern":"","type":"number"}
+
+            {"id":"","label":"Solid-Series-1","pattern":"","type":"number"},
+            {"id":"","label":"Solid-Series-2","pattern":"","type":"number"},
+
+            {"id":"","label":"Dotted-Series-1","pattern":"","type":"number"},
+            {"id":"","role":"certainty","type":"boolean"},
+            {"id":"","label":"Dotted-Series-2","pattern":"","type":"number"},
+            {"id":"","role":"certainty","type":"boolean"}
         ],
         "rows": [
-            {"c":[{"v":"Mushroooooms","f":null},{"v":3,"f":null}]},
-            {"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
-            {"c":[{"v":"Olives","f":null},{"v":2,"f":null}]},
-            {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
-            {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
+            {"c":[{"v":"Week 17","f":null},{"v":6,"f":null},{"v":5,"f":null},{"v":4,"f":null},{"v":false},{"v":3,"f":null},{"v":false}]},
+            {"c":[{"v":"Week 18","f":null},{"v":12,"f":null},{"v":11,"f":null},{"v":8,"f":null},{"v":false},{"v":7,"f":null},{"v":false}]},
+            {"c":[{"v":"Week 19","f":null},{"v":18,"f":null},{"v":15,"f":null},{"v":12,"f":null},{"v":false},{"v":12,"f":null},{"v":false}]},
+            {"c":[{"v":"Week 20","f":null},{"v":24,"f":null},{"v":20,"f":null},{"v":16,"f":null},{"v":false},{"v":14,"f":null},{"v":false}]}
         ]
     })
 
+    console.log('done with the sample api 1')
     req.flash('success_msg', 'You successfully retrieved all goals');
+    console.log('done with the sample api 2')
 });
 
 module.exports = router;
--- routes/visualize.js
+++ routes/visualize.js
@@ -1,4 +1,5 @@
 // https://bl.ocks.org/mbostock/3884955
+//
 
 var express = require('express');
 var router = express.Router();
@@ -7,7 +8,22 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     console.log('inside /visualize')
 
-    res.render('visualize/googlecharts5')
+    // res.render('visualize/googlecharts6')
+    // res.render('visualize/googlecharts6_option1_doesnotworkinterpolation', {datafromapi : {
+    //         "cols": [
+    //             {"id":"","label":"Topping","pattern":"","type":"string"},
+    //             {"id":"","label":"Slices","pattern":"","type":"number"}
+    //         ],
+    //         "rows": [
+    //             {"c":[{"v":"Mushrooms","f":null},{"v":3,"f":null}]},
+    //             {"c":[{"v":"Onions","f":null},{"v":1,"f":null}]},
+    //             {"c":[{"v":"Olives","f":null},{"v":2,"f":null}]},
+    //             {"c":[{"v":"Zucchini","f":null},{"v":1,"f":null}]},
+    //             {"c":[{"v":"Pepperoni","f":null},{"v":2,"f":null}]}
+    //         ]
+    //     }
+    // })
+    res.render('visualize/googlecharts8_option3')
 
     req.flash('success_msg', 'You successfully retrieved all goals');
 });
